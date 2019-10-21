c8afa6a17b49cc2c40445a7be6e26d04d9ab9aa7
Juwaini <juwaini@gmail.com>
2019-10-21 16:32:15 +0800

    use postgresql and change timezone

--- stream/settings.py
+++ stream/settings.py
@@ -78,29 +78,12 @@ WSGI_APPLICATION = 'stream.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
-
     'default': {
-        # MySQL database engine class.
-        'ENGINE': 'django.db.backends.mysql',
-        # MySQL database host ip.
-        'HOST': '127.0.0.1',
-        # port number.
-        'PORT': '3306',
-        # database name.
-        'NAME': 'ocst',
-        # user name.
-        'USER': 'root',
-        # password
-        'PASSWORD': 'password',
-        # connect options
-        'OPTIONS': {'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",},
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'emulator',
+        'USER': 'juwaini',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -127,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 LANGUAGE_CODE = 'en-us'
 
 # TIME_ZONE = 'UTC'
-TIME_ZONE = 'Asia/Seoul'
+TIME_ZONE = 'Asia/Kuala_Lumpur'
 
 USE_I18N = True
 
